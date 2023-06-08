156fb3074b8ae70ece0a1d011a024121422c7f2f
Bakhtiar Aleem <BakhtiarAleem@github.com>
2023-06-09 00:59:58 +0500

    unused data

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import { createApp } from 'vue'
 import '././assets/styles/main.scss'
 import App from './App.vue'
 import router from '././router'
-import 'vue3-easy-data-table/dist/style.css';
 
 
 createApp(App).use(router).mount('#app')
